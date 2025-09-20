bf40018db8fd0b9498a4ccf133d5fd357a72dcf3
Jhonny Bro <103278130+JhonnyB0098@users.noreply.github.com>
2025-09-20 17:23:55 +0000

    bug fixing

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,5 +1,31 @@
-from .python_sorts import bubble_sort, insertion_sort, selection_sort, merge_sort, quick_sort, heap_sort, radix_sort
+# from .python_sorts import bubble_sort, insertion_sort, selection_sort, merge_sort, quick_sort, heap_sort, radix_sort
 
+# algorithms = {
+#     "Bubble Sort": bubble_sort,
+#     "Insertion Sort": insertion_sort,
+#     "Selection Sort": selection_sort,
+#     "Merge Sort": merge_sort,
+#     "Quick Sort": quick_sort,
+#     "Heap Sort": heap_sort,
+#     "Radix Sort": radix_sort,
+# }
+
+# try:
+#     import cpp_sorts
+#     algorithms.update({
+#         "C++ Quick Sort": cpp_sorts.cpp_quick_sort,
+#         "C++ Merge Sort": cpp_sorts.cpp_merge_sort,
+#         "C++ Heap Sort": cpp_sorts.cpp_heap_sort,
+#         "C++ Radix Sort": cpp_sorts.cpp_radix_sort,
+#     })
+# except ImportError:
+#     pass
+from .python_sorts import (
+    bubble_sort, insertion_sort, selection_sort,
+    merge_sort, quick_sort, heap_sort, radix_sort
+)
+
+# Python algorithms
 algorithms = {
     "Bubble Sort": bubble_sort,
     "Insertion Sort": insertion_sort,
@@ -10,6 +36,7 @@ algorithms = {
     "Radix Sort": radix_sort,
 }
 
+# Try to import C++ implementations
 try:
     import cpp_sorts
     algorithms.update({
@@ -17,6 +44,10 @@ try:
         "C++ Merge Sort": cpp_sorts.cpp_merge_sort,
         "C++ Heap Sort": cpp_sorts.cpp_heap_sort,
         "C++ Radix Sort": cpp_sorts.cpp_radix_sort,
+        "C++ Bubble Sort": cpp_sorts.cpp_bubble_sort,
+        "C++ Insertion Sort": cpp_sorts.cpp_insertion_sort,
+        "C++ Selection Sort": cpp_sorts.cpp_selection_sort,
     })
 except ImportError:
+    # If C++ extension not built, fallback to Python only
     pass
